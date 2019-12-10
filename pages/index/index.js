e68857e2de063ab3927f62a5ee43147f918ff08d
guosongsong <623689478@qq.com>
2019-12-10 17:43:05 +0800

    送礼物页面banner图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,15 @@
 const app = getApp()
 Page({
     data: {
-
+        autoplay: true, // 是否自动切换
+        interval: 3000, // 自动切换时间间隔
+        duration: 800, // 滑动动画时长
+        indicatorDots: true, // 是否显示面板指示点
+        bannerImgaes: [
+            {image: '../../images/banner1.png'},
+            {image: '../../images/banner2.png'},
+            {image: '../../images/banner3.png'}
+        ]
     },
     onLoad: function () {
 
