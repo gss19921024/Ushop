3c9615db7cab54e680ea81dd5d5fa482926b2904
guosongsong <623689478@qq.com>
2019-12-12 16:38:46 +0800

    xxx

---
+++ pages/home/home.js
@@ -0,0 +1,27 @@
+
+Page({
+  data: {
+    time: 5
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    //5s后跳转
+    this.data.Time = setInterval(() => {
+      this.setData({
+        time: --this.data.time
+      })
+      if (this.data.time <= 0) {
+        clearInterval(this.data.Time)
+        this.goHome()
+      }
+    }, 1000)
+  },
+  goHome() {
+    clearInterval(this.data.Time)
+    wx.reLaunch({
+      url: '../index/index'
+    })
+  },
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 const app = getApp()
+
 Page({
     data: {
         autoplay: true, // 是否自动切换
@@ -9,7 +10,11 @@ Page({
             {image: '../../images/banner1.png'},
             {image: '../../images/banner2.png'},
             {image: '../../images/banner3.png'}
-        ]
+        ],
+        ballBottom: 150,
+        ballRight: 23,
+        screenHeight: 0,
+        screenWidth: 0,
     },
     bindTextAreaBlur: function(e) {
         this.setData({
@@ -18,6 +23,42 @@ Page({
         console.log(this.data.textareaValue)
     },
     onLoad: function () {
-
+        //获取屏幕宽高
+        var _this = this;
+        wx.getSystemInfo({
+            success: function (res) {
+                _this.setData({
+                    screenHeight: res.windowHeight,
+                    screenWidth: res.windowWidth,
+                });
+            }
+        });
+    },
+    ballMoveEvent: function (e) {
+        console.log('我被拖动了....')
+        var touchs = e.touches[0];
+        var pageX = touchs.pageX;
+        var pageY = touchs.pageY;
+        // console.log('pageX: ' + pageX)
+        // console.log('pageY: ' + pageY)
+//防止坐标越界,view宽高的一般
+        if (pageX < 30) return;
+        if (pageX > this.data.screenWidth - 30) return;
+        if (this.data.screenHeight - pageY <= 30) return;
+        if (pageY <= 30) return;
+//这里用right和bottom.所以需要将pageX pageY转换
+        var x = this.data.screenWidth - pageX - 30;
+        var y = this.data.screenHeight - pageY - 30;
+        // console.log('x: ' + x)
+        // console.log('y: ' + y)
+        this.setData({
+            ballBottom: y,
+            ballRight: x
+        });
+    },
+    //点击事件
+    ballClickEvent: function () {
+        console.log('点击了....')
     }
+
 })
